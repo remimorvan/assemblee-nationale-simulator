3ccc7fc3c1f790bf269be543be738b0051c10f42
Rémi Morvan <remi@morvan.xyz>
2026-01-17 14:54:16 +0100

    cards parsed from JSON

--- scripts/card.gd
+++ scripts/card.gd
@@ -7,13 +7,8 @@ var image_path: String # Text of the card
 var rng: RandomNumberGenerator
 
 # Called when the node enters the scene tree for the first time.
-func _init(_text: String, _effect_mean: Dictionary[String, float], _effect_std: Dictionary[String, float], _image_path: String) -> void:
-	text = _text
-	effect_mean = _effect_mean
-	effect_std = _effect_std
-	image_path = _image_path
-	rng = RandomNumberGenerator.new()
-	rng.randomize() 
+func _init() -> void:
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,6 +18,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func setup(_text: String, _effect_mean: Dictionary[String, float], _effect_std: Dictionary[String, float], _image_path: String) -> void:
+	text = _text
+	effect_mean = _effect_mean
+	effect_std = _effect_std
+	image_path = _image_path
+	rng = RandomNumberGenerator.new()
+	rng.randomize() 
+	
 # Returns the effect (delta on MP's approval's rate) of the card based on
 # an MP's political group.
 func get_approval_change(political_group: String) -> float:
---
+++ scripts/deck.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export var card:PackedScene # Utile pour instancier des MP
+var all_cards: Array[Node2D] = []
+
+# Called when the node enters the scene tree for the first time.
+func _init() -> void:
+	pass
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var file = FileAccess.open("res://assets/card/cards.json", FileAccess.READ)
+	var json = JSON.new()
+	if json.parse(file.get_as_text()) == OK:
+		var data_received = json.data
+		if typeof(data_received) == TYPE_ARRAY:
+			for data_card in data_received:
+				var new_card = card.instantiate()
+				var text: String = data_card["text"]
+				var effects = data_card["effects"]
+				var effects_mean: Dictionary[String, float] = {}
+				var effects_std: Dictionary[String, float] = {}
+				for party in effects:
+					effects_mean[party] = effects[party][0]
+					effects_std[party] = effects[party][1]
+				var image_path = data_card["image_path"]
+				new_card.setup(text, effects_mean, effects_std, image_path)
+				all_cards.append(new_card)
+		else:
+			print("Expected array")
+	else:
+		print("JSON Parse Error: ", json.get_error_message(), " in cards.json at line ", json.get_error_line())
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
