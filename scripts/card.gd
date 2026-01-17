fee03dcae6c047142ca72104b6f1ce7996d47a55
Rémi Morvan <remi@morvan.xyz>
2026-01-17 13:08:28 +0100

    card script : effect is normal law

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 var text: String # Text of the card
-var effect: Dictionary[String, float] # like social, french fries, economy
+var effect_mean: Dictionary[String, float] # political group -> mean effect
+var effect_std: Dictionary[String, float] # political group -> standard deviation of effect
 var image_path: String # Text of the card
 
 # Called when the node enters the scene tree for the first time.
-func _init(_text: String, _effect: Dictionary[String, float], _image_path: String) -> void:
+func _init(_text: String, _effect_mean: Dictionary[String, float], _effect_std: Dictionary[String, float] _image_path: String) -> void:
 	text = _text
-	effect = _effect
+	effect_mean = _effect_mean
+	effect_std = _effect_std
 	image_path = _image_path
 
 # Called when the node enters the scene tree for the first time.
