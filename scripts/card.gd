f68aef4926f7cf77ebce61b5476a775b34dd4b7f
Talessseed <timpicavet@gmail.com>
2026-01-17 11:57:59 +0100

    added fields to card

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+var text: String # Text of the card
+var effect: Dictionary[String, float] # like social, french fries, economy
+var image_path: String # Text of the card
+
+# Called when the node enters the scene tree for the first time.
+func _init(_text: String, _effect: Dictionary[String, float], _image_path: String) -> void:
+	text = _text
+	effect = _effect
+	image_path = _image_path
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
