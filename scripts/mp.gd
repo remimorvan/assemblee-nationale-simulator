5bb710cb367b41d17c7564f40a22192b5690f642
Talessseed <timpicavet@gmail.com>
2026-01-17 11:52:14 +0100

    added fields in mp

--- scripts/mp.gd
+++ scripts/mp.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
-var id: int # Numéro de siège
+var seat_id: int # Seat number
+var group_id: int # Polotical group number
+var affinity: Dictionary[String, float] # like social, french fries, economy
+
+# Called when the node enters the scene tree for the first time.
+func _init(_seat_id, _group_id, _affinity : Dictionary[String, float]) -> void:
+	seat_id = _seat_id
+	group_id = _group_id
+	affinity = _affinity
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
