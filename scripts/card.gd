84ea67ed098e5d63a7aff72b6fa3512ee1c74eda
Rémi Morvan <remi@morvan.xyz>
2026-01-17 18:50:19 +0100

    fix timtim's bêtise

--- scripts/card.gd
+++ scripts/card.gd
@@ -50,6 +50,7 @@ func _on_mouse_exited() -> void:
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		print("CLICK!")
 		for mp in get_tree().get_nodes_in_group("MP"):
 			mp.approval += get_approval_change(PoliticalGroup[mp.group_id])
 		# Remove card from hand
